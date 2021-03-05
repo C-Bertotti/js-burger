1d3a768008c1711499a970dad6bee765bcd0598f
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-05 17:45:42 +0100

    JS add for cycle

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 //Creo le variabili
 var calculate = document.getElementById('btn__calculate');
 var burgerName = document.getElementById('burger__name');
+var ingredients = document.getElementsByClassName('ingredient');
+var sum = 50;
+
 //Creo l'evento al click del bottone
 calculate.addEventListener('click',
     function() {
         //controllo che il nome del burger sia stato inserito
         if ( burgerName.value != '' ) {
-            console.log('ciao');
+            //controllo quali caselle sono checkate
+            for (var i = 0; i < ingredients.length; i++ ) {
+                //se le caselle sono checkate devo sommare il loro valore
+                if ( ingredients[i].checked ) {
+                    sum += parseInt(ingredients[i].value);
+                }
+            }
+            //stampo il nuovo valore della somma
+            console.log(sum);
         } else {
             alert('Ricordati di dare un nome al tuo burger!');
         }
-
     }   
 );
